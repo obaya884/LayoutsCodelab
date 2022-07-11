f8701193c08e5328d3188f68cdf9fa5b987cd958
obaya884 <taku3.884@gmail.com>
2022-07-11 20:31:34 +0900

    レイアウト修飾子の使用

--- app/src/main/java/com/example/layoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/layoutscodelab/MainActivity.kt
@@ -3,20 +3,13 @@ package com.example.layoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.layoutscodelab.ui.theme.LayoutsCodelabTheme
@@ -26,6 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
+                LayoutsCodelab()
             }
         }
     }
@@ -47,15 +41,16 @@ fun LayoutsCodelab() {
             )
         },
         bottomBar = {
-            BottomNavigation() {
-                
+            BottomNavigation {
+
             }
         }
     ) { innnerPadding ->
         BodyContent(
             Modifier
                 .padding(innnerPadding)
-                .padding(8.dp))
+                .padding(8.dp)
+        )
     }
 }
 
